37b7de4cd8d9228d54762aec0d556f7347c98b26
Dania <dfernandezmartinez@infomm.local>
2020-02-06 19:39:23 +0100

    Version 1.0 sesion 36

--- src/index.js
+++ src/index.js
@@ -1,12 +1,86 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Router, Link } from "@reach/router";
+// import Img form 'react-image';
+import Estilo1 from './Estilo1.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+/*
+const Aplicacion = () => {
+    return (
+        <div>
+            <h1>Manejo de rutas en React</h1>
+            <nav>
+                <Link to='/'>Inicio</Link>{" "}
+                <Link to="AcercaDe">Acerca De</Link>
+            </nav>
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+            <Router>
+                <Inicio path="/" />
+                <AcercaDe path="/AcercaDe" />
+            </Router>
+
+        </div>
+
+    )
+}
+
+const Inicio = () => {
+    return (
+        <div>
+            <h2>Estás en Inicio</h2>
+        </div>
+    )
+}
+
+const AcercaDe = () => (
+        <div>
+            <h2>Estás en Acerca De</h2>
+        </div>
+)
+
+ReactDOM.render(<Aplicacion/>, document.getElementById('root'));
+*/
+
+
+const Aplicacion = () => {
+    return (
+        <div>
+            <h1>Manejo de rutas en React</h1>
+            <nav>
+                <Link to='/'>Inicio</Link>{" "}
+                <Link to="Pagina1">Página 1</Link>{" "}
+                <Link to="Pagina2">Página 2</Link>
+            </nav>
+
+            <Router>
+                <Inicio path="/" />
+                <Pagina1 path="/Página1" />
+                <Pagina2 path="/Página2" />
+            </Router>
+
+        </div>
+
+    )
+}
+
+const Inicio = () => {
+    return (
+        <div className='inicio'>
+            <img src="./img/imagen1" alt="imagen 1" />
+        </div>
+    )
+}
+
+const Pagina1 = () => (
+        <div>
+            <h2>Página 1</h2>
+        </div>
+)
+
+const Pagina2 = () => (
+        <div>
+            <h2>Página 2</h2>
+        </div>
+)
+
+ReactDOM.render(<Aplicacion/>, document.getElementById('root'));
